ea401d51b503d1e13426e337f3db44a1633e5923
safaraliev <nurikonurik@gmail.com>
2026-02-25 00:07:12 -0600

    Fetch menu data from backend. TableViewController

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -9,10 +9,22 @@ import Foundation
 
 class NetworkManager{
     static let shared = NetworkManager()
-    
     private init(){}
     
-    func getFollowers(username: String, page: Int, completed: @escaping) -> [Follower]{
+    let baseURL = "https://api.github.com/users"
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+        
+        let endpoint = baseURL + "\(username)/followers?per_page=100$page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, "This username created an invalid request. Try again")
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            <#code#>
+        }
+        task.resume()
         
     }
 }
